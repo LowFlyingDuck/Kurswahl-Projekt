4eec1f1e866cb82c040d3c22c8240ff528460988
Nils Schwebel <datateam@gmx.de>
2021-10-15 20:51:51 +0200

    got app ready for developing phase

--- application/src/App.js
+++ application/src/App.js
@@ -1,8 +1,11 @@
+import './App.css'
+
 function App() {
   return (
-    <>
+    <div className="app">
       <h1>Selection coming soon...</h1>
-    </>
+      <p>Hold tight, while we are developing this app.</p>
+    </div>
   );
 }
 
