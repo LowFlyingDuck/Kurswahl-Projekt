767526f86310a296643072e7b5a56539692833e9
Nils Schwebel <datateam@gmx.de>
2021-10-15 06:34:45 +0000

    actually added build

---
+++ build/static/js/main.9c759dac.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpapplication=this.webpackJsonpapplication||[]).push([[0],{10:function(n,t,e){"use strict";e.r(t);var c=e(1),i=e.n(c),o=e(3),r=e.n(o),s=(e(8),e(0));var a=function(){return Object(s.jsx)(s.Fragment,{children:Object(s.jsx)("h1",{children:"Selection coming soon..."})})};r.a.render(Object(s.jsx)(i.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(n,t,e){}},[[10,1,2]]]);
+//# sourceMappingURL=main.9c759dac.chunk.js.map
